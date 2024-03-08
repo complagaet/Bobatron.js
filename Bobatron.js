a3d9137d5be6e72c06a518e08f41bfce2a841e37
Pavel Lukoyanov <148583866+complagaet@users.noreply.github.com>
2024-03-09 03:07:23 +0500

    First push

---
+++ Bobatron.js
@@ -0,0 +1,70 @@
+/*
+	БОБАТРОН ОТ ЛУКОЯНОВА ПАВЛА
+
+	Этот скрипт гениален. Он скругляет края практически любого элемента примерно так же, как у Эпла.
+
+	Для выбора цели добавьте класс "bobatron" к необходимому элементу,
+	Для запуска скрипта запустите bobatron.scanner().
+
+	По умолчанию скрипт создаёт SVG-маску (mask-image).
+
+	Атрибут "Bt-CM" - множитель радиуса скругления, значение по умолчанию 1.
+	Число меньше 1 делает радиус более маленьким.
+	Пример: <div class="anyClasses bobatron" Bt-CM="0.7"></div> 
+
+	Атрибут "Bt-Color" меняет значение background-image, маска не добавляется.
+	Параметр принимает значение цвета в шестнадцатеричном формате, например: #F92B63 или #1A1A1A99 (с альфа-каналом).
+	Пример: <div class="anyClasses bobatron" Bt-Color="#F92B63"></div>
+*/
+let bobatron = {
+	moveXY: (x, y, cornerMultiplier) => {
+		let path, viewBox
+		x = (x * (1 / cornerMultiplier)) - 91;
+		y = (y * (1 / cornerMultiplier)) - 91;
+		path = `M ${91 + x} 46 V 34 C ${91 + x} 7 ${84 + x} 0 ${57 + x} 0 H 34 C 7 0 0 7 0 34 V ${57 + y} C 0 ${84 + y} 7 ${91 + y} 34 ${91 + y} H ${57 + x} C ${84 + x} ${91 + y} ${91 + x} ${84 + y} ${91 + x} ${57 + y} Z`
+		viewBox = `0 0 ${91 + x} ${91 + y}`
+		return [path, viewBox]
+	},
+	toCSSbackground: (path_ViewBox, color, obj) => {
+		let css = `
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="${path_ViewBox[1]}">
+			<path d="${path_ViewBox[0]}" fill="${color}"/>
+		</svg>`
+		obj.style.backgroundImage = `url("data:image/svg+xml,${encodeURIComponent(css)}")`
+	},
+	toCSSmask: (path_ViewBox, obj) => {
+		let css = `
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="${path_ViewBox[1]}">
+			<path d="${path_ViewBox[0]}" fill=""/>
+		</svg>`, style = obj.getAttribute("style")
+		if (style) {
+			style = obj.getAttribute("style").split(";")
+			for(let i = 0; i < style.length; i++) {
+				if (style[i].includes("mask-")) { style[i] = "" }
+			}
+			let style_o = []
+			for (let o of style) {
+				if (o != "") { style_o.push(o) }
+			}
+			style = style_o.join(";")
+			if (style_o.length != 0) { style += "; " }
+		} 
+		else {
+			style = ""
+		}
+		obj.setAttribute("style", `${style}-webkit-mask-image: url("data:image/svg+xml,${encodeURIComponent(css)}"); -webkit-mask-repeat: no-repeat; mask-image: url("data:image/svg+xml,${encodeURIComponent(css)}"); mask-repeat: no-repeat`)
+	},
+	scanner: () => {
+		for (let i = 0, o = document.getElementsByClassName("bobatron"); i < o.length; i++) {
+			let cm = o[i].getAttribute("Bt-CM")
+			if (cm != null) { cm = Number(cm) }
+			else { cm = 1 }
+			try {
+				if (o[i].getAttribute("Bt-Color")) { bobatron.toCSSbackground(bobatron.moveXY(o[i].offsetWidth, o[i].offsetHeight, cm), o[i].getAttribute("Bt-Color"), o[i]) }
+				else { bobatron.toCSSmask(bobatron.moveXY(o[i].offsetWidth, o[i].offsetHeight, cm), o[i]) }
+				//console.log(o[i].offsetWidth, o[i].offsetHeight, o[i].getAttribute("Bt-Color"), cm)
+			}
+			catch(e) { console.log(e) }
+		}
+	}
+}
